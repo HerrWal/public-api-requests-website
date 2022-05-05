2da8e7d9de88833ecf99bcb200b60ffdaa396cf7
HerrWal <85037202+HerrWal@users.noreply.github.com>
2022-05-04 23:20:35 -0400

    Update scripts.js

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -34,7 +34,7 @@ async function getJSON(url) {
 async function getRandomUsers(url) {
     const userJSON = await getJSON(url);
     const contactInfo = userJSON.results.map(async (contact) => {
-        const image = contact.picture.thumbnail.toString();
+        const image = contact.picture.large.toString();
         const name = `${contact.name.first} ${contact.name.last}`;
         const email = contact.email;
         const city = `${contact.location.city}, ${contact.location.country}`;
@@ -44,3 +44,55 @@ async function getRandomUsers(url) {
 }
 
 console.log(getRandomUsers(randomUserURL));
+
+/**
+ * Gallery markup
+ */
+
+function generateHTML(data) {
+    console.log(data);
+    data.map((contact) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <div class="card-img-container">
+                <img class="card-img" src="${contact.image}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${contact.name}</h3>
+                <p class="card-text">${contact.email}</p>
+                <p class="card-text cap">${contact.city}</p>
+            </div>
+        `;
+        galleryDiv.insertAdjacentElement('beforeend', card);
+    });
+}
+
+/**
+ * Modal markup
+ */
+
+/* <div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+            <h3 id="name" class="modal-name cap">name</h3>
+            <p class="modal-text">email</p>
+            <p class="modal-text cap">city</p>
+            <hr>
+            <p class="modal-text">(555) 555-5555</p>
+            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+            <p class="modal-text">Birthday: 10/21/2015</p>
+        </div>
+    </div>
+
+    // IMPORTANT: Below is only for exceeds tasks 
+    <div class="modal-btn-container">
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>
+</div> */
+
+getRandomUsers(randomUserURL)
+    .then(generateHTML)
