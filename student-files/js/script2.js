c312f9d97eedc3c27d60fca82a124a6e5eecfc4b
HerrWal <85037202+HerrWal@users.noreply.github.com>
2022-07-12 08:02:42 -0400

    Create script2.js

---
+++ student-files/js/script2.js
@@ -0,0 +1,123 @@
+const randomUsersURL = "https://randomuser.me/api/?results=12";
+const galleryDiv = document.getElementById("gallery");
+const cards = document.getElementsByClassName('card');
+const body = document.querySelector('body');
+let selectedContact;
+let selectedContactIndex;
+
+const usersJSON = async (url) => {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (err) {
+    throw err;
+    console.error("Uh oh, something has gone wrong!");
+  }
+};
+
+const randomUsers = async (url) => {
+  const userJSON = await usersJSON(url);
+  const contactsInfo = userJSON.results.map((contact) => {
+    const image = contact.picture.large.toString();
+    const name = `${contact.name.first} ${contact.name.last}`;
+    const email = contact.email;
+    const city = `${contact.location.city}, ${contact.location.country}`;
+    const cell = contact.cell;
+    const address = contact.location.street;
+    const street = `${address.number} ${address.name}`;
+    const postCode = contact.location.postcode;
+    const dob = contact.dob.date.slice(0, 9);
+    return { image, name, email, city, cell, address, street, postCode, dob };
+  });
+  console.log(contactsInfo);
+  return contactsInfo;
+};
+
+function generateHTML(contactInfo) {
+  contactInfo.map((contact) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+                <div class="card-img-container">
+                    <img class="card-img" src="${contact.image}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${contact.name}</h3>
+                    <p class="card-text">${contact.email}</p>
+                    <p class="card-text cap">${contact.city}</p>
+                </div>
+            `;
+
+    galleryDiv.insertAdjacentElement("beforeend", card);
+  });
+}
+
+function createModal(contact) {
+  return `<div class="modal-container">
+  <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+        <img class="modal-img" src="${contact.image}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${contact.name}</h3>
+        <p class="modal-text">${contact.email}</p>
+        <p class="modal-text cap">${contact.city}</p>
+        <hr>
+        <p class="modal-text">${contact.cell}</p>
+        <p class="modal-text">${contact.street}, ${contact.city} ${contact.postCode}</p>
+        <p class="modal-text">Birthday:${contact.dob}</p>
+      </div>
+  </div>
+  <div class="modal-btn-container">
+      <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+      <button type="button" id="modal-next" class="modal-next btn">Next</button>
+  </div>
+</div>`;
+};
+
+function modalHandler(list, contact, index) {
+  const contactModal = createModal(contact);
+  galleryDiv.insertAdjacentHTML("afterend", contactModal);
+  const modalContainer = document.querySelector('.modal-container');
+  const modalCloseBtn = document.getElementById('modal-close-btn');
+  const modalPrevBtn = document.getElementById('modal-prev');
+  const modalNextBtn = document.getElementById('modal-next');  
+  if (modalCloseBtn) {
+    modalCloseBtn.addEventListener('click', (e) => {
+      if(modalCloseBtn.innerText === e.target.innerText) {
+        modalContainer.remove();
+      }
+    });
+  }  
+  modalPrevBtn.addEventListener('click', () => {
+    console.log('Previous contact');
+    modalContainer.remove();    
+  });
+  modalNextBtn.addEventListener('click', () => {
+    console.log('Next contact');
+    modalContainer.remove();
+    galleryDiv.insertAdjacentHTML("afterend", createModal(list[index + 1]));
+  });  
+};
+
+const loadPage = async () => {
+  try {
+    const contactsList = await randomUsers(randomUsersURL);
+    const contactCards = await generateHTML(contactsList);
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].addEventListener("click", (e) => {
+        if (cards[i].contains(e.target)) {
+          selectedContact = contactsList[i];
+          selectedContactIndex = i;
+          modalHandler(contactsList, selectedContact, selectedContactIndex);
+        }
+      });
+    }
+  
+    return contactCards;
+  } catch (err) {
+    throw err;
+    console.error(err);
+  }
+};
+
+loadPage();
