81a9b18b888433565f5d1ee48d9acec30bbd06a8
Juan Agustin Herrera Walwyn <85037202+HerrWal@users.noreply.github.com>
2022-07-19 19:23:19 -0400

    Update scripts.js

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -62,6 +62,7 @@ function createSearchBar() {
   searchContainer.insertAdjacentHTML("afterbegin", searchContainer);
 }
 
+
 function createModal(index) {
   const modal =
     `<div class="modal-container">
